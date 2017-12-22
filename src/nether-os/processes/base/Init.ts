0c14b878f10c091d86f7ae4286c061f2cd2d35b2
michael.rosmane <michael.rosmane@uniweb.eu>
2017-12-22 11:58:58 +0100

    Working out OS approach

---
+++ src/nether-os/processes/base/Init.ts
@@ -0,0 +1,5 @@
+import {Process} from "../../core/Process";
+
+export class InitProcess extends Process {
+    type = NETHEROS_PROCESS_INIT;
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,20 @@
 // type shim for nodejs' `require()` syntax
 declare const require: (module: string) => any;
+
+declare const NETHEROS_PROCESS_INIT = 'init';
+
+interface SerializedProcess {
+    name: string
+    priority: number
+    metaData: object
+    suspend: string | number | boolean
+    parent: string | boolean
+}
+
+interface ProcessTable{
+    [name: string]: Process
+}
+
+declare const processTypes = <{[type: string]: any}>{
+    'init': InitProcess,
+}
